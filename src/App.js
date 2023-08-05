fe3cf69704d52b470659e71f9b73ff637c613812
kerBamf <zach.pedigo@gmail.com>
2023-08-05 15:50:46 -0400

    Adding the bones for a new app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's get making us a new service, shall we?
         </p>
         <a
           className="App-link"
